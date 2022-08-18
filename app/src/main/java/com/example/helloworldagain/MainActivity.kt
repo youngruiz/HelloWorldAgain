18606bb29122da190006828c29e7240b76f305e9
youngruiz <rlod@protonmail.com>
2022-08-18 14:52:42 -0700

    Add functionality.

--- app/src/main/java/com/example/helloworldagain/MainActivity.kt
+++ app/src/main/java/com/example/helloworldagain/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.helloworldagain
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.helloButton)
+        button.setOnClickListener {
+            Log.v("HelloWorld", "Button Clicked")
+            Toast.makeText(this, "Hello to you too!", Toast.LENGTH_LONG).show()
+        }
     }
+
+
 }
